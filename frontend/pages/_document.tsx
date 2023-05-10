fd7befad5700594537da3d497e42243d00f686d2
Carl Lippert <carl.lippert@gmail.com>
2023-05-10 09:00:29 -0300

    force dark mode.

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="dark">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
